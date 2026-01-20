c3751831c72dd369477fd9fb67ebff4a8373e5af
Rakshit Agarwal <rakshitag13@gmail.com>
2026-01-20 21:17:39 +0530

    first 3 solved

---
+++ 1.two_sum.js
@@ -0,0 +1,13 @@
+var twoSum = function (nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const compliment = target - nums[i];
+
+    if (map.has(compliment)) {
+      return [i, map.get(compliment)];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+};
---
+++ 2.best_time_stock.js
@@ -0,0 +1,11 @@
+var maxProfit = function (prices) {
+  let curMin = prices[0];
+  let curMax = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    curMin = Math.min(prices[i], curMin);
+    curMax = Math.max(curMax, prices[i] - curMin);
+  }
+
+  return curMax;
+};
