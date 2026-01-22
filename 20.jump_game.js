158cfbd00e638947aafcd4e52dec3aceba3a9d05
Rakshit Agarwal <rakshitag13@gmail.com>
2026-01-22 22:04:34 +0530

    first 20 solved

---
+++ 20.jump_game.js
@@ -0,0 +1,12 @@
+// greedy approach
+var canJump = function (nums) {
+  let target = nums.length - 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (i + nums[i] >= target) {
+      target = i;
+    }
+  }
+
+  return target === 0;
+};
