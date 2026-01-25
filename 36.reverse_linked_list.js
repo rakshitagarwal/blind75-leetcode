8066ecc14dea33e6a0e2fbadb8e142545a769e14
Rakshit Agarwal <rakshitag13@gmail.com>
2026-01-25 22:16:03 +0530

    fist 40 solved

---
+++ 36.reverse_linked_list.js
@@ -0,0 +1,15 @@
+var reverseList = function (head) {
+  let prev = null;
+
+  while (head) {
+    let nextNode = head.next; // Store next node
+    head.next = prev; // Reverse pointer
+    prev = head; // Move prev forward
+    head = nextNode; // Move head forward
+  }
+
+  return prev;
+};
+
+// input: head = [1,2,3,4,5]
+// output: [5,4,3,2,1]
---
+++ 39.remove_nth_from_linkL_end.js
@@ -0,0 +1,22 @@
+var removeNthFromEnd = function (head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+
+  let left = dummy;
+  let right = head;
+
+  while (right && n > 0) {
+    right = right.next;
+    n -= 1;
+  }
+
+  while (right) {
+    left = left.next;
+    right = right.next;
+  }
+
+  left.next = left.next.next;
+  return dummy.next;
+};
+
+// example Input: head = [1,2,3,4,5], n = 2 Output: [1,2,3,5]
